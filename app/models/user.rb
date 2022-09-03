0c15c9c4b2435e9a2c949072be05923d0e391c28
Samuel Banya <sbanya@fastmail.com>
2022-09-03 11:30:22 -0400

    commit tree 5945f23a313760ae7f5ad79f88142095b8f68d7f

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  
+  # This is to establish the 'reviews' table as the 'join table':
+  has_many :reviews
+  # This is so that we can utilize the '.games()' method as a result of the association
+  # with the 'reviews' table:
+  has_many :games, through: :reviews
 end
