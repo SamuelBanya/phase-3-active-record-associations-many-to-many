cdb363f56b9f42903edd7fbfb3d3c673201e1cee
Samuel Banya <sbanya@fastmail.com>
2022-09-03 11:30:30 -0400

    Adding latest changes to assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,17 @@
 class Game < ActiveRecord::Base
+  # NOTE: This allows us to use the '.review()' method to access all of its associated reviews:
+  # NOTE: This is necessary to declare the 'join table' we need to provide for the
+  # 'has_many' macro:
   has_many :reviews
+  # Here we are providing the key-value pair, key: 'through' option, value: ':reviews' symbol
+  # which refers to the '#reviews' method above:
+  has_many :users, through: :reviews
+
+  # NOTE: The use of the second 'has_many' macro for the ':users, through: :reviews' section
+  # is to prevent us from having to write this exact SQL statement:
+  # SELECT "users".*
+  # FROM "users"
+  # INNER JOIN "reviews"
+  # ON "users"."id" = "reviews"."user_id"
+  # WHERE "reviews"."game_id" = 1
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :game
+  # NEW:
+  # This is used so that we can include the relationship to the 'User' class instance / 'users' table:
+  belongs_to :user
 end
